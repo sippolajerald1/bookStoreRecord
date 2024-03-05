fc2dfeeba1251c3a8d2f2fd5a06b31589c588b64
sippo <sippo@DESKTOP-4013P7G>
2024-03-05 18:26:20 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace recordDemo
+{
+    class Program
+    {
+        public record Bookstore(int ID, string Title, string Author, double Price);
+
+        public static void Main()
+        {
+            Bookstore book = new(1, "Call of the Wild", "Jack London", 20.00);
+            Console.WriteLine(book);
+            Bookstore book1 = new(2, "White Fang", "Jack London", 20.00);
+            Console.WriteLine(book1);
+            Bookstore book2 = new(3, "Conagher", "Louis LaMoore", 15.00);
+            Console.WriteLine(book2);
+        }
+    }
+}
